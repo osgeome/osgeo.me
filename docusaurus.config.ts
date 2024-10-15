6693b1d077b5295c4c0a8b37d638993420a164f2
Youssef-Harby <youssef_harby@yahoo.com>
2024-10-15 19:22:12 +0300

    Init

---
+++ docusaurus.config.ts
@@ -0,0 +1,143 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'OSGeo Middle East',
+  tagline: 'OSGeo الشرق الأوسط (Open Source Geospatial) هو مجتمع جديد مكرس لتقنيات الجغرافيا المكانية مفتوحة المصدر في منطقة الشرق الأوسط. هدفنا هو توحيد المطورين، ومتخصصي نظم المعلومات الجغرافية، وعلماء البيانات، والطلاب، وأي شخص مهتم بالخرائط والبيانات المكانية لتبادل الأفكار وإنشاء أدوات مفيدة. في فعالياتنا، يمكنك المشاركة في ورش العمل، والمحاضرات من الخبراء، والمشاريع، ومسابقات البرمجة، بالإضافة إلى بناء شبكة علاقات والتعاون مع الآخرين. انضم إلى OSGeo الشرق الأوسط لتتواصل، وتتعلم، وتساهم في بناء مجتمع جغرافي مكاني قوي في المنطقة!',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://osgeo.me',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'osgeome', // Usually your GitHub org/user name.
+  projectName: 'osgeo.me', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ar',
+    locales: ['ar', 'en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/osgeome/osgeo.me/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/osgeome/osgeo.me/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'OSGeo Middle East Logo',
+        src: 'img/osgeo-me-logo-cmyk.svg',
+        height: 100,
+        // width: 100,
+        style: {
+          height: '48px',
+        },
+
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/osgeome',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/QTvXu4Yb8g',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/osgeome',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} OSGeo.me`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ i18n/en/docusaurus-plugin-content-docs/current/About-OSGeo-Middle-East.md
@@ -0,0 +1,5 @@
+---
+sidebar_position: 1
+---
+
+OSGeo Middle East is a new community dedicated to open-source geospatial technologies in the Middle East. Our goal is to unite developers, GIS professionals, data scientists, students, and anyone interested in maps and location data to share ideas and create useful tools. At our events, you can participate in workshops, expert talks, projects, and hackathons while also networking and collaborating with others. Join OSGeo Middle East to connect, learn, and help build a strong geospatial community in the region!
